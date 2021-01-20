54aca46056d49ab462721faef8bba905d2f0f64a
Jefry Navas <jnavass@est.ups.edu.ec>
2021-01-20 16:03:09 -0500

    iniciar sesion y registrar usuario

---
+++ js/modal.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#loginModal').click(function(){
+        $('#loginModal').modal('show');
+        
+    })
+    $('#iniciar').click(function(){
+        $('#registerModal').modal('show');
+        $('#loginModal').modal('hide');
+        
+    })
+    $('#registrar').click(function(){
+        $('#loginModal').modal('show');
+        $('#registerModal').modal('hide');
+        
+        
+    })
+
+    $('#registerModal').click(function(){
+        $('#registerModal').modal('show');
+        
+    })
+    
+});
