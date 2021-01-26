32d519eab16252e72f576ed2bb83eb00718e9edc
Jefry Navas <jnavass@est.ups.edu.ec>
2021-01-26 10:27:27 -0500

    responsive

--- dashboard.php
+++ dashboard.php
@@ -29,8 +29,7 @@ session_start();
                 <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                    <a class="dropdown-item" href="#">Configuraciones</a>
-                    <div class="dropdown-divider"></div>
+                    
                     <a class="dropdown-item" href="salir.php">Cerrar Sesión</a>
                 </div>
             </li>
